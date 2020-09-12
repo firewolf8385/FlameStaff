c1222c08e57dd2f363e6e3a5ec3e79f7c8efd418
colby <colbykrawczyk10@gmail.com>
2020-09-12 07:02:01 -0400

    /uuid now works with offline players

--- src/main/java/com/github/firewolf8385/flamestaff/commands/UuidCMD.java
+++ src/main/java/com/github/firewolf8385/flamestaff/commands/UuidCMD.java
@@ -2,6 +2,7 @@ package com.github.firewolf8385.flamestaff.commands;
 
 import com.github.firewolf8385.flamestaff.utils.ChatUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,13 +15,7 @@ public class UuidCMD implements CommandExecutor {
             return false;
         }
 
-        Player t = Bukkit.getPlayer(args[0]);
-
-        if(t == null) {
-            ChatUtils.chat(sender, "&c&l(&7!&c&l) &cThat player is not online.");
-            return true;
-        }
-
+        OfflinePlayer t = Bukkit.getOfflinePlayer(args[0]);
         ChatUtils.chat(sender, "&a&l(&7!&a&l) &aUUID of &f" + t.getName() + " &ais &f" + t.getUniqueId() + "&a.");
 
         return true;
